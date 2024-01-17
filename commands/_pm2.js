31a94bc4a1986fd60ca8860d5f83c88b68694ee3
vishwamihi <98463616+vishwamihi@users.noreply.github.com>
2024-01-17 16:07:55 +0530

    🍃V̴i̴s̴h̴w̴a̴ m̴d̴🍃

--- commands/_pm2.js
+++ commands/_pm2.js
@@ -1,15 +1,15 @@
 /**
- Copyright (C) 2022.
+ Copyright (C) 2024.
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
+ * @project_name : VISHWA-MD
+ * @author : VishwaMihiranga <https://github.com/vishwamihiranga>
+ * @description : Vishwa-MD,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
-
 //---------------------------------------------------------------------------
+
 const {cmd,tlang,sleep} = require('../lib')
 cmd({
         pattern: "restart",
@@ -20,7 +20,7 @@ cmd({
     async(Void, citel,text,{ isCreator }) => {
    if (!isCreator) return citel.reply(tlang().owner)
             const { exec } = require("child_process")
-            citel.reply('Restarting')
+            citel.reply('🗣️Restarting....')
             await sleep(2000)
             exec('pm2 restart all')
     }
