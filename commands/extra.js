0062eb9bd530634ab1773eb10080d9297e6fc18e
vishwamihi <98463616+vishwamihi@users.noreply.github.com>
2024-01-17 15:48:39 +0530

    🍃V̴i̴s̴h̴w̴a̴ m̴d̴🍃

--- commands/extra.js
+++ commands/extra.js
@@ -1,15 +1,15 @@
-/*
- *
- Copyright (C) 2022.
+/**
+ Copyright (C) 2024.
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
+ * @project_name : VISHWA-MD
+ * @author : VishwaMihiranga <https://github.com/vishwamihiranga>
+ * @description : Vishwa-MD,A Multi-functional whatsapp bot.
  * @version 0.0.6
- *
- */
+ **/
+//---------------------------------------------------------------------------
+
 
 const {cmd} = require('../lib')
 const PastebinAPI = require("pastebin-js");
@@ -22,7 +22,7 @@ cmd({
     },
     async(Void, citel) => {
         if(!citel.quoted) return citel.reply('Please quote any text to get link.')
-        let data = await pastebin.createPaste(citel.quoted.text, "Secktor-Pastebin")
+        let data = await pastebin.createPaste(citel.quoted.text, "VISHWA-MD-Pastebin")
         citel.reply('_Here is your link._\n'+data)
     }
 );
