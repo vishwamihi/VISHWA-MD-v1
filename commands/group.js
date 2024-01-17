242e404a8663b6a10d82b706b886eefa36b9ee31
vishwamihi <98463616+vishwamihi@users.noreply.github.com>
2024-01-17 16:16:21 +0530

    🍃V̴i̴s̴h̴w̴a̴ m̴d̴🍃

--- commands/group.js
+++ commands/group.js
@@ -91,7 +91,7 @@ cmd({
         citel.reply(`*Check your Pm ${tlang().greet}*`);
         await Void.sendMessage(`${citel.sender}`, {
             image: log0,
-            caption: `*Group Name: VISHWA-MD-Support*\n*Group Link:* https://chat.whatsapp.com/Bl2F9UTVU4CBfZU6eVnrbC`,
+            caption: `*Group Name: VISHWA-MD-Support*\n*Group Link:* https://chat.whatsapp.com/GSJ3mmEI1g11dj0WHgCcC4 `,
         });
 
     }
