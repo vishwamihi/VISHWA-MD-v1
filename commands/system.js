f89222543a012b20a592f339d60968471397e548
vishwamihi <98463616+vishwamihi@users.noreply.github.com>
2024-01-17 15:15:56 +0530

    🍃V̴i̴s̴h̴w̴a̴ m̴d̴🍃

--- commands/system.js
+++ commands/system.js
@@ -1,13 +1,15 @@
+
 /**
- Copyright (C) 2022.
+ Copyright (C) 2024.
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
+ * @project_name : VISHWA-MD
+ * @author : VishwaMihiranga <https://github.com/vishwamihiranga>
+ * @description : Vishwa-MD,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
+//---------------------------------------------------------------------------
 
 const { addnote,cmd, sck1, delnote, allnotes, delallnote, tlang, botpic, runtime, prefix, Config ,sleep} = require('../lib')
 const { TelegraPh } = require('../lib/scraper')   
@@ -48,7 +50,7 @@ cmd({
                 headerType: 4,
                 contextInfo: {
                     externalAdReply: {
-                        title: 'Secktor Session',
+                        title: 'VISHWA-MD Session',
                         body: 'Get you Session ID',
                         thumbnail: log0,
                         mediaType: 2,
@@ -243,7 +245,7 @@ cmd({
             desc: "is bot alive??"
         },
         async(Void, citel, text, isAdmins) => {
-            let alivemessage = Config.ALIVE_MESSAGE || `*A bot developed by SamPandey001.*`
+            let alivemessage = Config.ALIVE_MESSAGE || `*A bot developed by VISHWA.*`
             const alivtxt = `
 *Hello, ${citel.pushName},*
 _This is  ${tlang().title}._
