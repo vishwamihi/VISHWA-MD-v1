9bbf783ef7a1e00490cf0f21f5a9ade755c8fcef
vishwamihi <98463616+vishwamihi@users.noreply.github.com>
2024-01-17 15:49:04 +0530

    🍃V̴i̴s̴h̴w̴a̴ m̴d̴🍃

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,3 +1,16 @@
+/**
+ Copyright (C) 2024.
+ Licensed under the  GPL-3.0 License;
+ You may not use this file except in compliance with the License.
+ It is supplied in the hope that it may be useful.
+ * @project_name : VISHWA-MD
+ * @author : VishwaMihiranga <https://github.com/vishwamihiranga>
+ * @description : Vishwa-MD,A Multi-functional whatsapp bot.
+ * @version 0.0.6
+ **/
+//---------------------------------------------------------------------------
+
+
 const {cmd } = require('../lib')
 cmd({
         pattern: "insta",
